45050313c6fbb0a33178bb17ff960662484d74f4
Michal Butkiewicz <wexford3@gmail.com>
2017-10-18 15:08:14 +0200

    JZ4ZC-12 Add parsing from AztecVehicle to JSON

--- src/main/java/pl/isa/autoparts/aztec/AztecJsonParser.java
+++ src/main/java/pl/isa/autoparts/aztec/AztecJsonParser.java
@@ -1,5 +1,6 @@
 package pl.isa.autoparts.aztec;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
@@ -11,5 +12,12 @@ public class AztecJsonParser {
         ObjectMapper mapper = new ObjectMapper();
 
         return mapper.readValue(jsonData, AztecVehicle.class);
+    }
+
+    public static String aztecVehicleToJson(AztecVehicle aztecVehicle) throws JsonProcessingException {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        return mapper.writeValueAsString(aztecVehicle);
     }
 }
