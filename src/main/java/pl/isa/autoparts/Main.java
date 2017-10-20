1361d49a3d09c48b584c862a6d7f4bc78c5c165b
steveyakob <jakubesuszynski@gmail.com>
2017-10-20 22:23:07 +0200

    Print parents of searched phrase

--- src/main/java/pl/isa/autoparts/Main.java
+++ src/main/java/pl/isa/autoparts/Main.java
@@ -1,8 +1,6 @@
 package pl.isa.autoparts;
 
 
-
-import pl.isa.autoparts.TextMenu;
 import pl.isa.categories.AllegroItem;
 import pl.isa.categories.Parser;
 import pl.isa.categories.TreeOperations;
@@ -12,11 +10,9 @@ import java.util.ArrayList;
 public class Main {
 
 
-
     public static void main(String[] args) {
         int chosenOption;
-        int czesciSamochodoweId = 620;
-        int czesciSamochodowePosition = 0;
+
         int phraseId;
         AllegroItem czesciSamochodowe;
         ArrayList<AllegroItem> allegroList = new ArrayList<AllegroItem>();
@@ -35,26 +31,13 @@ public class Main {
 
 
 
+        TreeOperations treeOperations = new TreeOperations();
+        treeOperations.findCzesciSamochodowePosition(allegroList);
+        treeOperations.setSearchedPhrase("Silniczki szyb"); //for example
+        phraseId = treeOperations.findPhrase();
+        treeOperations.printParent(allegroList, phraseId);
+        treeOperations.printWholeTree(0, treeOperations.getCzesciSamochodowe());
 
-
-        for (int i = 0; i < allegroList.size(); i++) { //find collection position of category "Części Samochodowe"
-            if (allegroList.get(i).getId() == czesciSamochodoweId) {
-                czesciSamochodowePosition = i;
-                break;
-            }
-        }
-
-        czesciSamochodowe = allegroList.get(czesciSamochodowePosition);
-
-        czesciSamochodowe.setChildren(allegroList);
-
-        TreeOperations treeOperations = new TreeOperations(czesciSamochodowe);
-
-        //treeOperations.printWholeTree(0, czesciSamochodowe);
-        treeOperations.searchedPhrase("Silniczki szyb"); //for example
-
-        phraseId = treeOperations.findPhrase(czesciSamochodowe);
-        System.out.println(phraseId);
     }
 }
 
--- src/main/java/pl/isa/categories/TreeOperations.java
+++ src/main/java/pl/isa/categories/TreeOperations.java
@@ -1,15 +1,35 @@
 package pl.isa.categories;
 
+import java.util.ArrayList;
+
 public class TreeOperations {
 
+    static int czesciSamochodoweId = 620;
+    private int czesciSamochodowePosition = 0;
     private String phrase;
     private int phraseId;
-    private AllegroItem czesciSamochodowe = new AllegroItem();
 
-    public TreeOperations(AllegroItem allegroItem) {
-        this.czesciSamochodowe = allegroItem;
+
+    public AllegroItem getCzesciSamochodowe() {
+        return czesciSamochodowe;
     }
 
+    private AllegroItem czesciSamochodowe = new AllegroItem();
+    private AllegroItem czesciSamochodoweClipboard = new AllegroItem();
+
+
+    public void findCzesciSamochodowePosition(ArrayList<AllegroItem> allegroList) {
+        for (int i = 0; i < allegroList.size(); i++) { //find collection position of category "Części Samochodowe"
+            if (allegroList.get(i).getId() == czesciSamochodoweId) {
+                czesciSamochodowePosition = i;
+                break;
+            }
+        }
+        czesciSamochodowe = allegroList.get(czesciSamochodowePosition);
+        czesciSamochodowe.setChildren(allegroList);
+        czesciSamochodoweClipboard = czesciSamochodowe; // method findPhrase overwrites czesciSamochodowe object so we have to be able to recover it
+
+    }
 
     public void printWholeTree(int stars, AllegroItem czesciSamochodowe) {
         stars++;
@@ -22,21 +42,32 @@ public class TreeOperations {
         }
     }
 
-    public void searchedPhrase(String phrase) {
+    public void setSearchedPhrase(String phrase) {
         this.phrase = phrase;
     }
 
-    public int findPhrase(AllegroItem czesciSamochodowe) {
+    public int findPhrase() {   //returns ID of searched category
         for (AllegroItem item : czesciSamochodowe.getChildren()) {
             if (item.getName().equals(phrase)) {
-               phraseId = item.getId();
-               break;
+                phraseId = item.getId();
+                break;
             } else {
-                findPhrase(item);
+                this.czesciSamochodowe = item;
+                findPhrase();
             }
         }
-
+        czesciSamochodowe = czesciSamochodoweClipboard;
         return phraseId;
+    }
+
+    public void printParent(ArrayList<AllegroItem> allegroList, int parentId) { //prints parents by giving to a a method id of first parent
+        for (AllegroItem item : allegroList) {
+            if (item.getId() == parentId) {
+                System.out.println(item.getName());
+                printParent(allegroList, item.getParent());
+                break;
+            }
+        }
     }
 }
 
