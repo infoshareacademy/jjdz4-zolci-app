70f1213e444b104219ca9fc93a1e14341b62671c
Michal Butkiewicz <wexford3@gmail.com>
2017-10-22 20:37:08 +0200

    JZ4ZC-15 Extend AztecJsonParser class

--- src/main/java/pl/isa/autoparts/aztec/AztecJsonParser.java
+++ src/main/java/pl/isa/autoparts/aztec/AztecJsonParser.java
@@ -4,15 +4,15 @@ import pl.isa.autoparts.tools.JsonParser;
 
 import java.io.IOException;
 
-public class AztecJsonParser {
+public class AztecJsonParser extends JsonParser {
 
     public static AztecVehicle parseAztecFromURL(String url) throws IOException {
 
-        return (AztecVehicle) JsonParser.parseJsonFromURL(url, new AztecVehicle());
+        return (AztecVehicle) parseJsonFromURL(url, new AztecVehicle());
     }
 
     public static AztecVehicle parseAztecFromFile(String fileName) throws IOException {
 
-        return (AztecVehicle) JsonParser.parseJsonFromFile(fileName, new AztecVehicle());
+        return (AztecVehicle) parseJsonFromFile(fileName, new AztecVehicle());
     }
 }
