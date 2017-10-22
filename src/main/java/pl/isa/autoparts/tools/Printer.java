ded20bd064be1b25122ae0218c680d186d070a3f
Michal Butkiewicz <wexford3@gmail.com>
2017-10-22 20:48:15 +0200

    JZ4ZC-15 Change access to Printer finals

--- src/main/java/pl/isa/autoparts/tools/Printer.java
+++ src/main/java/pl/isa/autoparts/tools/Printer.java
@@ -2,10 +2,10 @@ package pl.isa.autoparts.tools;
 
 public abstract class Printer {
 
-    public static final String NORMAL = "\u001B[0m";
-    public static final String YELLOW = "\u001B[33m";
-    public static final String BLUE = "\u001B[34m";
-    public static final String RED = "\u001B[31m";
+    protected static final String NORMAL = "\u001B[0m";
+    protected static final String YELLOW = "\u001B[33m";
+    protected static final String BLUE = "\u001B[34m";
+    protected static final String RED = "\u001B[31m";
 
     public static void println(String text) {
 
