a59f5348cb4691ba3b8470914ac16cc7621fa863
kmull <krzysztofmuller1@gmail.com>
2017-10-22 19:08:22 +0200

    first commit

---
+++ src/main/java/pl/isa/autoparts/questions/Group.java
@@ -0,0 +1,4 @@
+package pl.isa.autoparts.questions;
+
+public class Group {
+}
---
+++ src/main/java/pl/isa/autoparts/questions/ParserQuestonsXML.java
@@ -0,0 +1,10 @@
+package pl.isa.autoparts.questions;
+
+
+public class ParserQuestonsXML {
+
+    public static void main(String[] args) {
+
+    }
+
+}
