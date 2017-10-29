a1f627bc4eaa4a2c69e5d668696205316a1c1d1c
ansid88 <anna.sidz88@gmail.com>
2017-10-29 20:31:22 +0100

    linki do allegro

---
+++ src/main/java/pl/isa/autoparts/allegro/AllegroLinks.java
@@ -0,0 +1,14 @@
+package pl.isa.autoparts.allegro;
+import pl.isa.autoparts.categories.TreeOperations;
+
+public class AllegroLinks {
+    public static void main(String[] args) {
+
+        TreeOperations treeOperations = new TreeOperations();
+        treeOperations.printWholeTree();
+
+
+
+    }
+
+}
