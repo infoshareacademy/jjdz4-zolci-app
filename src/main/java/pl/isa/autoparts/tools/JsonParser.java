adaebc013009adb6e3e4b34f508a00e2edab3b56
Michal Butkiewicz <wexford3@gmail.com>
2017-11-21 23:32:18 +0100

    JZ4ZC-62 Rewrite json parsers to use generic types

--- src/main/java/pl/isa/autoparts/tools/JsonParser.java
+++ src/main/java/pl/isa/autoparts/tools/JsonParser.java
@@ -1,45 +1,30 @@
 package pl.isa.autoparts.tools;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class JsonParser {
 
-    public static <T> T parseFromGeneric(String s) throws IOException {
-        T jsonObject = null;
-        ObjectMapper mapper = new ObjectMapper();
+    private JsonParser() {
 
-        return mapper.readValue(new URL(s), (Class<T>) jsonObject.getClass());
     }
 
-    public static Object parseJsonFromURL(String url, Object object) throws IOException {
+    public static <T> T parseJsonFromURL(String url, T jsonObject) throws IOException {
 
         ObjectMapper mapper = new ObjectMapper();
-
-        return mapper.readValue(new URL(url), object.getClass());
+        return mapper.readValue(new URL(url), (Class<T>) jsonObject.getClass());
     }
 
-    public static Object parseJsonFromFile(String fileName, Object object) throws IOException {
+    public static <T> T parseJsonFromFile(String fileName, T jsonObject) throws IOException {
 
-        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-        InputStream file = classloader.getResourceAsStream(fileName);
-
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream file = classLoader.getResourceAsStream(fileName);
         if (file == null) throw new IOException();
 
         ObjectMapper mapper = new ObjectMapper();
-
-        return mapper.readValue(file, object.getClass());
-    }
-
-    public static String toJsonString(Object object) throws JsonProcessingException {
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        return mapper.writeValueAsString(object);
+        return mapper.readValue(file, (Class<T>) jsonObject.getClass());
     }
 }
