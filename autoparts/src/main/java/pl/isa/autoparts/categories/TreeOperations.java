b6ff20d5b9addd0f8ed698568757fdc98024ba3e
steveyakob <jakubesuszynski@gmail.com>
2018-01-27 18:57:34 +0100

    refactor

--- autoparts/src/main/java/pl/isa/autoparts/categories/TreeOperations.java
+++ autoparts/src/main/java/pl/isa/autoparts/categories/TreeOperations.java
@@ -6,13 +6,17 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 
 public class TreeOperations {
-    private int czesciSamochodowePosition = 0;
     Logger logger = LoggerFactory.getLogger(TreeOperations.class.getName());
 
     private ArrayList<AllegroItem> allegroList = new ArrayList<>();
     private ArrayList<AllegroItem> partsList = new ArrayList<>();
     private AllegroItem czesciSamochodowe = new AllegroItem();
 
+    public ArrayList<AllegroItem> getSimilarList() {
+        return similarList;
+    }
+
+    private ArrayList<AllegroItem> similarList = new ArrayList<>();
     public TreeOperations() {
         Parser parser = new Parser();
         allegroList = parser.getAllegroList();
@@ -20,17 +24,18 @@ public class TreeOperations {
 
     }
 
-    private void createCarPartsList(ArrayList<AllegroItem> allegroList, AllegroItem parent) {
+    private void createCarPartsList(int parentId) {
         for (AllegroItem item : allegroList) {
-            if (item.getId() == parent.getId()){
+            if (item.getParent() == parentId){
                 partsList.add(item);
-                createCarPartsList(allegroList, item);
+                createCarPartsList(item.getId());
             }
         }
     }
 
     private void findCzesciSamochodowePosition() {
         int czesciSamochodoweId = 620;
+        int czesciSamochodowePosition = 0;
         for (int i = 0; i < allegroList.size(); i++) { //find collection position of category "Części Samochodowe"
             if (allegroList.get(i).getId() == czesciSamochodoweId) {
                 czesciSamochodowePosition = i;
@@ -38,15 +43,21 @@ public class TreeOperations {
             }
         }
         czesciSamochodowe = allegroList.get(czesciSamochodowePosition);
-        createCarPartsList(allegroList, czesciSamochodowe);
+        createCarPartsList(czesciSamochodowe.getId());
     }
 
-    public AllegroItem findCarPartCategory(String phrase){
+//    public AllegroItem findCarPartCategory(String phrase){
+//        for(AllegroItem item : partsList){
+//            if(item.getName().startsWith(phrase))
+//                return item;
+//        }
+//        return null;
+//    }
+    public void findCarPartCategory(String phrase){
         for(AllegroItem item : partsList){
             if(item.getName().startsWith(phrase))
-                return item;
+                similarList.add(item);
         }
-        return null;
     }
 
     public AllegroItem findParent(AllegroItem child){
--- autopartsJee/src/main/java/pl/isa/autopartsJee/tools/WebLinkGenerator.java
+++ autopartsJee/src/main/java/pl/isa/autopartsJee/tools/WebLinkGenerator.java
@@ -5,26 +5,34 @@ import pl.isa.autoparts.categories.TreeOperations;
 import pl.isa.autoparts.tools.StringNormalizer;
 import sun.reflect.generics.tree.Tree;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class WebLinkGenerator {
 
     private String itemName = new String();
 
-
+    ArrayList<String> linksList = new ArrayList<>();
+    Map<String, String> nameLink = new Map<String, String>();
     public String getItemName() {
         return itemName;
     }
 
-    public String generateLink(String category, TreeOperations treeOperations) {
+    public void generateLink(String category, TreeOperations treeOperations) {
         Integer catID;
         AllegroItem parent;
         AllegroItem categoryItem;
         StringNormalizer stringNormalizer = new StringNormalizer();
+
         try {
-            categoryItem = treeOperations.findCarPartCategory(category);
-            parent = treeOperations.findParent(categoryItem);
-            return "https://allegro.pl/kategoria/" + stringNormalizer.normalize(parent.getName())
-                    + "-" + stringNormalizer.normalize(categoryItem.getName()) + "-" + categoryItem.getId();
+            treeOperations.findCarPartCategory(category);
+            ArrayList<AllegroItem> similarList = treeOperations.getSimilarList();
+            for (AllegroItem item : similarList) {
+                parent = treeOperations.findParent(item);
+                linksList.add("https://allegro.pl/kategoria/" + stringNormalizer.normalize(parent.getName())
+                        + "-" + stringNormalizer.normalize(item.getName()) + "-" + item.getId());
 //            logger.info("Link generated");
+            }
         } catch (IndexOutOfBoundsException e) {
 //            logger.error("No category parent or category not found, error in link generating");
 
