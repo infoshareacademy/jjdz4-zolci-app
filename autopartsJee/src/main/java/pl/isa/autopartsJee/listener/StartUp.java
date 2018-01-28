4226ce573d2efb57f32d0a41d2fe7b4e6a2638a3
steveyakob <jakubesuszynski@gmail.com>
2018-01-28 17:30:23 +0100

    add Listener

---
+++ autopartsJee/src/main/java/pl/isa/autopartsJee/listener/StartUp.java
@@ -0,0 +1,20 @@
+package pl.isa.autopartsJee.listener;
+
+import pl.isa.autopartsJee.dao.TreeOperationsRepositoryDao;
+
+import javax.inject.Inject;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class StartUp implements ServletContextListener {
+    @Inject
+    TreeOperationsRepositoryDao dao;
+
+    public void contextInitialized(ServletContextEvent e) {
+        dao.getRepository();
+    }
+
+    public void contextDestroyed(ServletContextEvent e) {
+
+    }
+}
