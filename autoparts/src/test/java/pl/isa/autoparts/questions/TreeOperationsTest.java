41effab160a25a3c7082b7b05700bd2a988dbf5e
steveyakob <jakubesuszynski@gmail.com>
2018-02-11 18:24:08 +0100

    begin tests

---
+++ autoparts/src/test/java/pl/isa/autoparts/questions/TreeOperationsTest.java
@@ -0,0 +1,21 @@
+package pl.isa.autoparts.questions;
+
+import org.junit.Before;
+import org.junit.Test;
+import pl.isa.autoparts.categories.AllegroItem;
+import pl.isa.autoparts.categories.TreeOperations;
+
+public class TreeOperationsTest {
+    TreeOperations treeOperations;
+
+    @Before
+    public void init() {
+        treeOperations = new TreeOperations();
+    }
+
+    @Test
+    public void should_return_parent() {
+        AllegroItem allegroItem = new AllegroItem();
+
+    }
+}
