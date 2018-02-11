fdb7d48143f2b0f2684abc01b0c93e3c69f4cc4f
steveyakob <jakubesuszynski@gmail.com>
2018-02-11 16:53:19 +0100

    change logs to log4j2

--- autoparts/src/main/java/pl/isa/autoparts/Main.java
+++ autoparts/src/main/java/pl/isa/autoparts/Main.java
@@ -2,6 +2,7 @@ package pl.isa.autoparts;
 
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class Main {
 
@@ -9,6 +10,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         TextMenu textMenu = new TextMenu();
         textMenu.options();
+        LocalDateTime localDateTime = LocalDateTime.now();
     }
 }
 
