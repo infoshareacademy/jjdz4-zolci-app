e5bf8fc6814317546cc1ffea4d2987fac4c36d25
steveyakob <jakubesuszynski@gmail.com>
2018-02-18 15:23:59 +0100

    deployable rest api

--- raportmodule/src/main/java/pl.isa.raportmodule/ApiService.java
+++ raportmodule/src/main/java/pl.isa.raportmodule/ApiService.java
@@ -5,6 +5,7 @@ import pl.isa.raportmodule.repository.LogRepository;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
@@ -15,10 +16,9 @@ public class ApiService {
     @Inject
     LogRepository logRepository;
 
-    @POST
+    @GET
     @Path("/addlog")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response addLog(Log log) {
+    public Response addLog() {
 //        logRepository.addSingleLog(log);
         return Response.ok("test").build();
     }
