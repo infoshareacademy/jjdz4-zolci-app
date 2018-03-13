7a5ba4353b5b2f7d945e0521d149e59bf062049a
Michał Butkiewicz <michal-butkiewicz@outlook.com>
2018-03-13 17:41:48 +0100

    JZ4ZC-55 Add more functionality for created forms

--- autopartsJee/src/main/java/pl/isa/autopartsJee/vehiclesearch/VehicleSearchServlet.java
+++ autopartsJee/src/main/java/pl/isa/autopartsJee/vehiclesearch/VehicleSearchServlet.java
@@ -2,13 +2,9 @@ package pl.isa.autopartsJee.vehiclesearch;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import pl.isa.autoparts.tools.JsonParser;
 import pl.isa.autoparts.vehiclesearch.Vehicle;
-import pl.isa.autoparts.vehiclesearch.VehicleData;
 import pl.isa.autoparts.vehiclesearch.VehicleSearch;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,57 +21,73 @@ public class VehicleSearchServlet extends HttpServlet{
 
     private final Logger logger = LoggerFactory.getLogger(VehicleSearchServlet.class.getName());
 
-    private HttpServletRequest request;
-    private HttpServletResponse response;
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
-        request = req;
-        response = res;
-
-        Map<String, String> makes = new TreeMap<>();
+        Map<String, String> makes;
 
         try {
-            makes = VehicleSearch.getVehicleMakesFromApi(API_LINK);
+            Vehicle vehicle = VehicleSearch.getVehicleFromApi(API_LINK);
+            makes = vehicle.getNamesAndApi();
         } catch (IOException e) {
             String errorMessage = "Could not parse vehicle names from database.";
             logger.error(errorMessage);
-            forwardToPageWithError(errorMessage);
+            new PageController(req, res).forwardWithError(errorMessage);
+            return;
         }
 
         req.setAttribute("makes", makes);
-        forwardToPage("vehicle-search.jsp");
+        new PageController(req, res).forward("vehicle-search.jsp");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) {
 
-        request = req;
-        response = res;
+        PageController pageController = new PageController(req, res);
 
-        Optional<String> make = Optional.ofNullable(request.getParameter("make"));
+        Optional<String> engine = Optional.ofNullable(req.getParameter("engine"));
+        if (engine.isPresent()) {
+            logger.info("ENGINE NOT IMPLEMENTED YET");
+        }
+
+        Optional<String> model = Optional.ofNullable(req.getParameter("model"));
+        if (model.isPresent()) {
+
+            Map<String,String> engines;
+            try {
+                Vehicle vehicle = VehicleSearch.getVehicleFromApi(model.get());
+                engines = vehicle.getNamesAndApi();
+            } catch (IOException e) {
+                String errorMessage = "Could not load engine names from API";
+                logger.error(errorMessage);
+                pageController.forwardWithError(errorMessage);
+                return;
+            }
+
+            req.setAttribute("engines", engines);
+            pageController.forward("vehicle-search-step2.jsp");
+            return;
+        }
+
+        Optional<String> make = Optional.ofNullable(req.getParameter("make"));
         if (make.isPresent()) {
 
+            Map<String,String> models;
+            try {
+                Vehicle vehicle = VehicleSearch.getVehicleFromApi(make.get());
+                models = vehicle.getNamesAndApi();
+            } catch (IOException e) {
+                String errorMessage = "Could not load model names from API";
+                logger.error(errorMessage);
+                pageController.forwardWithError(errorMessage);
+                return;
+            }
+
+            req.setAttribute("models", models);
+            pageController.forward("vehicle-search-step1.jsp");
+            return;
         }
-    }
 
-    private void forwardToPage(String page) {
-
-        RequestDispatcher dispatcher = request.getRequestDispatcher(page);
-        try {
-            dispatcher.forward(request, response);
-
-        } catch (ServletException e) {
-            logger.error("Servlet request dispatcher error." + e.toString());
-        } catch (IOException e) {
-            logger.error("IO Servlet request dispatcher error." + e.toString());
-        }
-    }
-
-    private void forwardToPageWithError(String errorMessage) {
-
-        request.setAttribute("errorMessage", errorMessage);
-        forwardToPageWithError("vehicle-search.jsp");
+        pageController.forwardWithError("No option has been selected");
     }
 }
