0bb17935ebca932fd9b6f411cfb1d43829b0e8b9
Michał Butkiewicz <michal-butkiewicz@outlook.com>
2018-03-13 21:11:02 +0100

    JZ4ZC-55 Update VS servlet functionality

--- autopartsJee/src/main/java/pl/isa/autopartsJee/vehiclesearch/VehicleSearchServlet.java
+++ autopartsJee/src/main/java/pl/isa/autopartsJee/vehiclesearch/VehicleSearchServlet.java
@@ -2,28 +2,36 @@ package pl.isa.autopartsJee.vehiclesearch;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pl.isa.autoparts.tools.JsonParser;
 import pl.isa.autoparts.vehiclesearch.Vehicle;
+import pl.isa.autoparts.vehiclesearch.VehicleData;
 import pl.isa.autoparts.vehiclesearch.VehicleSearch;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Optional;
-import java.util.TreeMap;
 
 @WebServlet("vehicle-search")
 public class VehicleSearchServlet extends HttpServlet{
 
-    private static final String API_LINK = "/api/v2";
+    private final Logger LOG = LoggerFactory.getLogger(VehicleSearchServlet.class.getName());
 
-    private final Logger logger = LoggerFactory.getLogger(VehicleSearchServlet.class.getName());
+    private static final String API_LINK = "/api/v2";
+    private static final String MAKE_API = "makeApi";
+    private static final String MAKE_NAME = "makeName";
+    private static final String MODEL_API = "modelApi";
+    private static final String MODEL_NAME = "modelName";
+    private static final String ENGINE_NAME = "engineName";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+        PageController pageController = new PageController(req, res);
         Map<String, String> makes;
 
         try {
@@ -31,39 +39,66 @@ public class VehicleSearchServlet extends HttpServlet{
             makes = vehicle.getNamesAndApi();
         } catch (IOException e) {
             String errorMessage = "Could not parse vehicle names from database.";
-            logger.error(errorMessage);
-            new PageController(req, res).forwardWithError(errorMessage);
+            LOG.error(errorMessage);
+            pageController.forwardWithError(errorMessage);
             return;
         }
 
         req.setAttribute("makes", makes);
-        new PageController(req, res).forward("vehicle-search.jsp");
+        pageController.forward("vehicle-search.jsp");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) {
 
+        HttpSession session = req.getSession();
         PageController pageController = new PageController(req, res);
 
         Optional<String> engine = Optional.ofNullable(req.getParameter("engine"));
         if (engine.isPresent()) {
-            logger.info("ENGINE NOT IMPLEMENTED YET");
+
+            // TODO Finish this nicely
+
+
+            OptionValue value;
+            Vehicle vehicle;
+            try {
+                value = parseParameters(engine.get());
+                vehicle = VehicleSearch.getVehicleFromApi(value.getApi());
+            } catch (IOException e) {
+                String errorMessage = "Could not load Vehicle from given API";
+                LOG.error(errorMessage);
+                pageController.forwardWithError(errorMessage);
+                return;
+            }
+
+            for (VehicleData vd : vehicle.getData()) {
+                if (vd.getLink().equals(value.getApi())) {
+                    session.setAttribute(ENGINE_NAME, value.getName());
+                    pageController.forward("vehicle-search-step3.jsp");
+                    return;
+                }
+            }
         }
 
         Optional<String> model = Optional.ofNullable(req.getParameter("model"));
         if (model.isPresent()) {
 
+            OptionValue value;
             Map<String,String> engines;
             try {
-                Vehicle vehicle = VehicleSearch.getVehicleFromApi(model.get());
+                value = parseParameters(model.get());
+                Vehicle vehicle = VehicleSearch.getVehicleFromApi(value.getApi());
                 engines = vehicle.getNamesAndApi();
             } catch (IOException e) {
                 String errorMessage = "Could not load engine names from API";
-                logger.error(errorMessage);
+                LOG.error(errorMessage);
                 pageController.forwardWithError(errorMessage);
                 return;
             }
 
+            session.setAttribute(MODEL_NAME, value.getName());
+            session.setAttribute(MODEL_API, value.getApi());
             req.setAttribute("engines", engines);
             pageController.forward("vehicle-search-step2.jsp");
             return;
@@ -72,22 +107,31 @@ public class VehicleSearchServlet extends HttpServlet{
         Optional<String> make = Optional.ofNullable(req.getParameter("make"));
         if (make.isPresent()) {
 
+            OptionValue value;
             Map<String,String> models;
             try {
-                Vehicle vehicle = VehicleSearch.getVehicleFromApi(make.get());
+                value = parseParameters(make.get());
+                Vehicle vehicle = VehicleSearch.getVehicleFromApi(value.getApi());
                 models = vehicle.getNamesAndApi();
             } catch (IOException e) {
                 String errorMessage = "Could not load model names from API";
-                logger.error(errorMessage);
+                LOG.error(errorMessage);
                 pageController.forwardWithError(errorMessage);
                 return;
             }
 
+            session.setAttribute(MAKE_NAME, value.getName());
+            session.setAttribute(MAKE_API, value.getApi());
             req.setAttribute("models", models);
             pageController.forward("vehicle-search-step1.jsp");
             return;
         }
 
         pageController.forwardWithError("No option has been selected");
+    }
+
+    private OptionValue parseParameters(String value) throws IOException {
+
+        return JsonParser.parseFromString(value, OptionValue.class);
     }
 }
