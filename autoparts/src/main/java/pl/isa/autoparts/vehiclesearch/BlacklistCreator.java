36c070cc150f323e48ce2ee3effbd02aa69606cd
Michał Butkiewicz <michal-butkiewicz@outlook.com>
2018-03-22 03:36:48 +0100

    JZ4ZC-55 Implement vehicle makes blacklist creator

--- autoparts/src/main/java/pl/isa/autoparts/vehiclesearch/BlacklistCreator.java
+++ autoparts/src/main/java/pl/isa/autoparts/vehiclesearch/BlacklistCreator.java
@@ -3,23 +3,53 @@ package pl.isa.autoparts.vehiclesearch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class BlacklistCreator {
 
     private static final Logger LOG = LoggerFactory.getLogger(BlacklistCreator.class.getName());
+    private static final String FILE_NAME = "VehicleMakesBlacklist";
 
     public static void main(String[] args) {
 
-        LOG.info("Executing vehicle blacklist creator...");
+        LOG.info("Executing vehicle blacklist creator.");
+        LOG.info("May take sooooooome time...");
 
-        Set<String> blacklist;
+        Set<String> blacklist = new TreeSet<>(Arrays.asList("AR, BDR, CDR"));
+
         try {
             blacklist = searchForEmptyMakes(getMakesApis());
         } catch (IOException e) {
             LOG.error("Data parsing was unsuccessful");
         }
+
+        Optional<Set<String>> black = Optional.ofNullable(blacklist);
+        if (black.isPresent()) {
+
+            try {
+                fileWrite(black.get());
+            } catch (IOException e) {
+                LOG.error("Could not write to file");
+            }
+        }
+    }
+
+    public static Set<String> read() throws IOException, ClassNotFoundException {
+
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream fis = classLoader.getResourceAsStream(FILE_NAME);
+        if (fis == null) throw new IOException();
+
+        Set<String> blacklist;
+        try (ObjectInputStream ois = new ObjectInputStream(fis)) {
+            blacklist = (Set<String>) ois.readObject();
+            ois.close();
+        }
+
+        return blacklist;
     }
 
     private static Map<String,String> getMakesApis() {
@@ -41,7 +71,7 @@ public class BlacklistCreator {
 
     private static Set<String> searchForEmptyMakes(Map<String,String> makesApis) throws IOException {
 
-        Set<String> blacklist = null;
+        Set<String> blacklist = new TreeSet<>();
         for (Map.Entry<String,String> ma : makesApis.entrySet()) {
             Vehicle vehicleModelLevel;
             vehicleModelLevel = VehicleSearch.getVehicleFromApi(ma.getValue());
@@ -75,7 +105,12 @@ public class BlacklistCreator {
         return true;
     }
 
-    private static boolean isEmptyOrNull(String name) {
-        return name.isEmpty() || name == null;
+    private static void fileWrite(Set<String> blacklist) throws IOException {
+
+        FileOutputStream fos = new FileOutputStream(FILE_NAME);
+        try (ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(blacklist);
+            oos.close();
+        }
     }
 }
